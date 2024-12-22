e2e9438a0aa576f424ee1a0d41d4655ccc4f0813
Pubudu11 <pubuduganganodya@gmail.com>
2024-12-22 23:50:29 +0530

    upedate

--- react-route-app/src/component/ListEmployeeComponent.tsx
+++ react-route-app/src/component/ListEmployeeComponent.tsx
@@ -10,7 +10,7 @@ const ListEmployeeComponent: React.FC = () => {
       <button onClick={() => navigate("/add-employee")} style={{ padding: "10px 20px", margin: "0 10px" }}>
         Add Employee
       </button>
-      <button onClick={() => navigate("/update-employee/1")} style={{ padding: "10px 20px", margin: "0 10px" }}>
+      <button onClick={() => navigate("/update-employee/:id")} style={{ padding: "10px 20px", margin: "0 10px" }}>
         Update Employee
       </button>
     </>
